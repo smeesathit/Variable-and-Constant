05a8296dc45179cc3ca9e3fa85c5f0c5a8cef050
student04 <smeesathit@gmail.com>
2017-06-12 15:14:34 +0700

    after break

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//This Comment หรือ นี่คือ Comment
+// นี่คือการประกาศตัวแปร แบบ ไม่สามารถเปลี่ยนค่าได้
+let myConstant = 123  //Implicit Constant 123
+//myConstant = 456 cannot chang let
+// นี่คือการประกาศตัวแปร แบบ เปลี่ยนค่าได้
+var intNumber = 123 //Implicit variable intNumber = 123
+intNumber = 456
+
+//Explicit นี่คือการประกาศตัวแปร แบบ กำหนดค่า Datatype
+let myConstant1: Int = 123
+
+// Display on Console
+print(intNumber)  //Display only Value
+// Display String and Value
+print("intNumber ==> \(intNumber)")
+
