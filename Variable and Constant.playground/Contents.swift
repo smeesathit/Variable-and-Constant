696ec0cf5514cbb0057ce3e73beae1d9467789f7
student04 <smeesathit@gmail.com>
2017-06-12 16:14:47 +0700

    varOK

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -18,3 +18,71 @@ print(intNumber)  //Display only Value
 // Display String and Value
 print("intNumber ==> \(intNumber)")
 
+// Declare explicit String constant
+// Specify data type tobe String
+let strName: String = "Doraemon"
+
+// Declare implicit String constant
+let strSurname = "Japan"
+
+// Concate string
+var strOfficer = strName + " " + strSurname
+strOfficer = "Mr." + strOfficer
+
+// Number Operation
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA * intB
+answer = intA / intB
+
+var douA = Double(intA)
+var douB = Double(intB)
+
+var answer2:Double = douA / douB
+
+
+var answer3: Double =  Double (intA) / Double (intB)
+
+
+var myStatus = true // implicit boolean declaration
+var myStatus2: Bool = false // explicit boolean declaration
+
+// print on console ==> ถ้าเอา 5 + 6 = 11
+
+// print("5 + 6 = \(5+6)")
+// String messages
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strSpace = " "
+let strAdd = "บวก"
+let strDivide = "หาร"
+
+// Int variables
+let intNum1 = 5
+let intNum2 = 6
+
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strAdd + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(intNum1 + intNum2)" )
+
+print("")
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strDivide + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(Double(intNum1) / Double(intNum2))" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
